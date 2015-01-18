8470d6d90f38b8acb035db14dcb521852dca295f
snarles <snarles@stanford.edu>
2015-01-17 19:53:29 -0800

    writing some functions

--- charles2.m
+++ charles2.m
@@ -1,6 +1,15 @@
 %% 
 
-stim = csvread('valid_stim.csv',1,0);
-stim_index = csvread('valid_index.csv',1,0);
-response = csvread('valid_v1.csv',1,0);
-locations = csvread('v1_locations.csv',1,0);
+load('vox100stuff.mat');
+%%
+
+sel_vox = vox100s;
+ind = 1;
+cur_block = vox100s{ind};
+stim_block = seqVal(:,ind); % there are 12 stimuli
+nt = length(cur_block); % total number of 
+ns = n/4; % total number of stimuli
+sub_stim_block = subsample(stim_block,4);
+%%
+
+len_hrf = 30;
--- subsample.m
+++ subsample.m
@@ -2,6 +2,6 @@ function w = subsample( v, period )
 % returns v[1, 1+period, 1+2*period,..]
 l = length(v);
 k = l/period;
-w = v[1+((1:k)-1)*period];
+w = v(1+((1:k)-1)*period);
 end
 
