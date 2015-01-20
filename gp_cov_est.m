3e2a9690a67bfddd2df0dff0e8aa2645004d675f
snarles <snarles@stanford.edu>
2015-01-19 17:50:28 -0800

    covariance estimation

---
+++ gp_cov_est.m
@@ -0,0 +1,12 @@
+function covmat = gp_cov_est( resid, basis_mat, eps, nits)
+% estimates covariance matrix using Gaussian process model
+% Inputs:
+%  - resid, 1-dimensional observed gaussian process
+%  - basis_mat, functions in KL expansion
+%  - eps, small number
+%  - nits, nits*epsilon control smoothing used for KL variance terms
+
+
+
+end
+
