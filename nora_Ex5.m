11d3f48c6a67082fd9bd3ebc5161002bf6fdca2b
Nora Brackbill <nbrackbill@gmail.com>
2015-02-22 23:00:36 -0800

    Partway through Part C

--- nora_Ex5.m
+++ nora_Ex5.m
@@ -144,7 +144,82 @@ title('ISI Distribution')
 
 %% Real neural data
 load('spike_dat.mat')
+n_bins=500;
+n_trials=182;
 
+spikes=zeros(n_bins, n_trials);
+
+for k=1:8
+    
+    for i=1:n_trials
+        spikes(:,i)=trial(i,k).spikes;
+    end
+    
+    % Plot the trials
+    figure(1);
+    subplot(3,3,plot_order(k))
+    % title(['s=' num2str(k) '\pi/4'])
+    imagesc(spikes(:,1:5)'); colormap(flipud(gray));
+    xlim([0 n_bins])
+    ylim([0 6])
+    set(gca,'XTick',[0 1000],'XTickLabel',[0 1])
+    
+    % Histogram
+    figure(2);
+    subplot(3,3,plot_order(k))
+    tmp=reshape(conv(spikes(:), ones(20,1), 'same'), n_bins, n_trials);
+    spike_hist = sum(tmp,2)/n_trials;
+    stairs(spike_hist(10:20:end))
+    ylim([0 1])
+    
+    % Tuning curve
+    temp=sum(spikes);
+    emp_firing_rate(k)=mean(temp);
+    emp_var(k)=std(temp);
+    
+    % Count distribution
+    figure(4);
+    subplot(3,3,plot_order(k))
+    [counts,centers] = hist(temp);
+    lambda_hat=poissfit(temp);
+    plot(centers, poisspdf(round(centers),lambda_hat), 'k')
+    hold on
+    plot(centers, counts/(n_trials), 'r')
+    hold off
+    xlim([0 20])
+    ylim([0 0.5])
+    
+    % ISI
+    figure(6);
+    subplot(3,3,plot_order(k))
+    ISI=diff(find(spikes))/1000;
+    mean_ISI(k)=mean(ISI);
+    Cv_ISI(k)=std(ISI)/mean(ISI);
+    [counts, centers] = hist(ISI);
+    mu_hat=expfit(ISI);
+    plot(centers, exppdf(centers,mu_hat), 'k')
+    hold on
+    plot(centers, counts/(n_trials), 'r')
+    hold off
+    xlim([0 0.5])
+    ylim([0 20])
+
+end
+
+% Tuning Curve
+figure(3);
+hold on
+errorbar(emp_firing_rate, emp_var/sqrt(n_trials), 'r')
+%plot(firing_rate((0:7)*pi/4), 'k')
+legend('Simulated Firing Rate', 'True Tuning Curve')
+title('Tuning Curve')
+
+% Fano Factor
+figure(5);
+hold on
+scatter(emp_firing_rate, emp_var.^2)
+plot([0 7],[0 7])
+title('Mean Firing Rate vs. Variance')
 
 
 
